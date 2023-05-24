656ea0667804fc455776366173617583137bae9e
Guillem Córdoba <guillem.cordoba@gmail.com>
2023-05-24 14:01:58 +0200

    Updates

---
+++ _develop/scripts/release.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+rm -r .release
+rm -r dist
+mkdir .release
+mkdir .release/quill
+
+npm run build
+webpack --config _develop/webpack.config.js --env.minimize
+cp dist/quill.core.css dist/quill.bubble.css dist/quill.snow.css dist/quill.js dist/quill.core.js dist/quill.min.js dist/quill.min.js.map .release/quill/
+
---
+++ core/emitter.js
@@ -0,0 +1,79 @@
+import EventEmitter from 'eventemitter3';
+import logger from './logger';
+
+const debug = logger('quill:events');
+const EVENTS = ['selectionchange', 'mousedown', 'mouseup', 'click'];
+const EMITTERS = [];
+const supportsRootNode = ('getRootNode' in document);
+
+EVENTS.forEach(function(eventName) {
+  document.addEventListener(eventName, (...args) => {
+    EMITTERS.forEach((em) => {
+      em.handleDOM(...args);
+    });
+  });
+});
+
+
+class Emitter extends EventEmitter {
+  constructor() {
+    super();
+    this.listeners = {};
+    EMITTERS.push(this);
+    this.on('error', debug.error);
+  }
+
+  emit() {
+    debug.log.apply(debug, arguments);
+    super.emit.apply(this, arguments);
+  }
+
+  handleDOM(event, ...args) {
+    const target = (event.composedPath ? event.composedPath()[0] : event.target);
+    const containsNode = (node, target) => {
+      if (!supportsRootNode || target.getRootNode() === document) {
+        return node.contains(target);
+      }
+
+      while (!node.contains(target)) {
+        const root = target.getRootNode();
+        if (!root || !root.host) {
+          return false;
+        }
+        target = root.host;
+      }
+
+      return true;
+    };
+
+    (this.listeners[event.type] || []).forEach(function({ node, handler }) {
+      if (target === node || containsNode(node, target)) {
+        handler(event, ...args);
+      }
+    });
+  }
+
+  listenDOM(eventName, node, handler) {
+    if (!this.listeners[eventName]) {
+      this.listeners[eventName] = [];
+    }
+    this.listeners[eventName].push({ node, handler })
+  }
+}
+
+Emitter.events = {
+  EDITOR_CHANGE        : 'editor-change',
+  SCROLL_BEFORE_UPDATE : 'scroll-before-update',
+  SCROLL_OPTIMIZE      : 'scroll-optimize',
+  SCROLL_UPDATE        : 'scroll-update',
+  SELECTION_CHANGE     : 'selection-change',
+  TEXT_CHANGE          : 'text-change'
+};
+Emitter.sources = {
+  API    : 'api',
+  SILENT : 'silent',
+  USER   : 'user'
+};
+
+
+export default Emitter;
